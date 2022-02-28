afb15a6b43119d3100d941bcf5a3ceec6478cf1b
csatio <csatio@yahoo.com.br>
2022-02-28 11:23:04 -0300

    rf

--- app.py
+++ app.py
@@ -20,22 +20,22 @@ st.markdown("# Vai Corrê!!!")
 
 def vai_corre():
 
-  json_file = open('model/model.json', 'r')
-  loaded_model_json = json_file.read()
-  json_file.close()
-  loaded_model = model_from_json(loaded_model_json)
+  #json_file = open('model/model.json', 'r')
+  #loaded_model_json = json_file.read()
+  #json_file.close()
+  #loaded_model = model_from_json(loaded_model_json)
   # load weights into new model
-  loaded_model.load_weights("model/model.h5")
-  print("Loaded model from disk")
+  #loaded_model.load_weights("model/model.h5")
+  #print("Loaded model from disk")
   
-  #model = pickle.load(open('model/best_model_xgb.pkl', 'rb'))
+  model = pickle.load(open('model/best_model_rf.pkl', 'rb'))
 
   #model_predictor = model.load_model('model-2022-02-23.json')
 
 
   dia_semana = st.text_input('0 = dia normal ...  1 = fim de semana', 1)
 
-  volume_semanal_ant = st.text_input('Volume semanal atual', '20')
+  volume_semanal_ant = st.text_input('Volume semanal atual', '25')
 
   volume_semanal_ant2 = st.text_input('Volume semana anterior', '20')
 
@@ -44,7 +44,7 @@ def vai_corre():
           'vol_semanal_ant2': [volume_semanal_ant2]
           }
 
-  treino = loaded_model.predict(pd.DataFrame(data))
+  treino = model.predict(pd.DataFrame(data))
 
 
   st.markdown("--------------------")
