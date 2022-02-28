2a084398c09a2db6bf2956afd7b1467e500913c3
csatio <csatio@yahoo.com.br>
2022-02-28 10:44:04 -0300

    first deep learning

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import requests
 import os
 import pickle
 
+from keras.models import model_from_json
 
 
 image = Image.open('IMG-20171231-WA0001.jpg')
@@ -18,19 +19,32 @@ st.markdown("# Vai Corrê!!!")
 
 
 def vai_corre():
-  
-  model = pickle.load(open('model/best_model_xgb.pkl', 'rb'))
-  
 
-  dia_semana = st.text_input('Dia da semana 00 = segunda ...  06 = domingo', '06')
+  json_file = open('model/model.json', 'r')
+  loaded_model_json = json_file.read()
+  json_file.close()
+  loaded_model = model_from_json(loaded_model_json)
+  # load weights into new model
+  loaded_model.load_weights("model/model.h5")
+  print("Loaded model from disk")
+  
+  #model = pickle.load(open('model/best_model_xgb.pkl', 'rb'))
 
-  volume_semanal = st.text_input('Volume semanal atual', '8')
+  #model_predictor = model.load_model('model-2022-02-23.json')
+
+
+  dia_semana = st.text_input('0 = dia normal ...  1 = fim de semana', 1)
+
+  volume_semanal_ant = st.text_input('Volume semanal atual', '20')
+
+  volume_semanal_ant2 = st.text_input('Volume semana anterior', '20')
 
   data = {'dia_semana': dia_semana,
-          'vol_semanal_ant': [volume_semanal]
+          'vol_semanal_ant': [volume_semanal_ant],
+          'vol_semanal_ant2': [volume_semanal_ant2]
           }
 
-  treino = model.predict(pd.DataFrame(data))
+  treino = loaded_model.predict(pd.DataFrame(data))
 
 
   st.markdown("--------------------")
